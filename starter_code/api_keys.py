059badd63e0bfbf67a7cec31d19a340203e41fa8
tingjennings <tingzuo258@gmail.com>
2021-04-23 23:41:29 -0400

    add

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "53604c03ed05a5060b85789b9c4e6633"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCHLylvz1Nb4W-Q8ONxr8WUNEs8zFsjZug"
